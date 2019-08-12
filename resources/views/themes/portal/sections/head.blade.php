6e1c900bcc793e6c64632a7a9e597784f36bba19
Sergio Galindo Mundo <sgalindo.smartsoft@gmail.com>
2019-08-11 23:36:29 -0700

    englis

--- resources/views/themes/portal/sections/head.blade.php
+++ resources/views/themes/portal/sections/head.blade.php
@@ -4,11 +4,9 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="apple-touch-icon" sizes="57x57" href="//img1.wsimg.com/ux/favicon/apple-icon-57x57.png">
-
+    
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="format-detection" content="telephone=yes">
-    <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
 
     <link href="{{asset('cms/inicio/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('cms/inicio/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
